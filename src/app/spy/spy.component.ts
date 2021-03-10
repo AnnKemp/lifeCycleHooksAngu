61e30017c31eec8e7be2e75eb22fa6fdfb82539c
AnnKemp <ann.kemp@scarlet.be>
2021-03-10 23:41:56 +0100

    adding spy directive and logger.service.ts and spy component

---
+++ src/app/spy/spy.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { LoggerService } from '../logger.service';
+
+@Component({
+  selector: 'app-spy',
+  templateUrl: './spy.component.html',
+  styleUrls: ['./spy.component.css'],
+  //providers: [loggerService]  // het pad hier naar toe klopt niet helemaal boven heb ik een puntje moeten bijvoegen hier moet waarschijnlijk ook zoiets gebeuren?
+})
+export class SpyComponent{
+  newName = 'Herbie';
+  heroes: string[] = ['WindStorm', 'Magneta'];
+
+  constructor(public logger: LoggerService) { }
+
+  addHero(){
+
+  }
+
+reset(){
+
+}
+// hier moet ik nog code toevoegen in de functies etc. + nog een spy.directive.ts
+}
